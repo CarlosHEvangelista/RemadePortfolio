050b7a66018a6ef80057b1e662dfed553044c5a1
unknown <carloshenrqueevangelista@gmail.com>
2023-01-26 08:46:14 -0300

    .

---
+++ IconToggleSystem.js
@@ -0,0 +1,7 @@
+IconForToggleSystem.addEventListener('click', function () {
+  if (ProjectsTitle.classList == 'ShowLink') {
+    ProjectsTitle.classList.remove('ShowLink')
+  } else {
+    ProjectsTitle.classList.add('ShowLink')
+  }
+})
