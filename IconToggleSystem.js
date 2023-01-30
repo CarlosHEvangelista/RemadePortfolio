a6869d056764c4d39a0cd1d70d08c0b1f9cdcb72
unknown <carloshenrqueevangelista@gmail.com>
2023-01-30 08:57:36 -0300

    Finish HTML

--- IconToggleSystem.js
+++
@@ -1,7 +0,0 @@
-IconForToggleSystem.addEventListener('click', function () {
-  if (ProjectsTitle.classList == 'ShowLink') {
-    ProjectsTitle.classList.remove('ShowLink')
-  } else {
-    ProjectsTitle.classList.add('ShowLink')
-  }
-})
